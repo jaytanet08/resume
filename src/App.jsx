7f2a250cc62be54f3b0fb7e3e27ba3c07f1233eb
jaytanet08 <154994714+jaytanet08@users.noreply.github.com>
2024-06-20 13:37:01 +0700

    v3

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import PortfolioPage from './pages/PortfolioPage';
 import Contact from './pages/Contact';
 import Education from './pages/Education';
 import Experience from './pages/Experience';
-
+import '../src/setting/i18n'; 
 
 const App = () => (
   <>
@@ -24,3 +24,26 @@ const App = () => (
 );
 
 export default App;
+
+
+// import React from 'react';
+// import { useTranslation } from 'react-i18next';
+
+// function App() {
+//   const { t, i18n } = useTranslation();
+
+//   const changeLanguage = (lng) => {
+//     i18n.changeLanguage(lng);
+//   };
+
+//   return (
+//     <div>
+//       <h1>{t('welcome')}</h1>
+//       <p>{t('thankYou')}</p>
+//       <button onClick={() => changeLanguage('en')}>English</button>
+//       <button onClick={() => changeLanguage('th')}>ไทย</button>
+//     </div>
+//   );
+// }
+
+// export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import './setting/i18n.js'; 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
   <App />
